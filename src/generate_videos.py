a873f25d3e7a05a06125391be595c084dcd9a7e5
kimryan0416 <36388915+kimryan0416@users.noreply.github.com>
2025-11-20 06:45:23 -0500

    generate_videos.py now puts operations within a try-except-finally case to potentially handle issues with temp dirs not being deleted if an operation fails

--- src/generate_videos.py
+++ src/generate_videos.py
@@ -40,6 +40,7 @@ assert os.path.exists(args.input_metadata), "Input .json metadata file not detec
 pipeline = rs.pipeline()
 config = rs.config()
 config.enable_device_from_file(args.input_bag, repeat_playback=False)
+profile = None
 
 # Generate some helper functions for helping us generate temporary output directories for each stream
 def mkdirs(query_dir:str, delete_existing:bool=True):
@@ -77,7 +78,14 @@ with open(args.input_metadata, "r") as f:
             continue
 
         config.enable_stream(rs_type, stream['width'], stream['height'], rs_format, stream['fps'])
-        streams[stream['type']] = { **stream, "rs.type":rs_type, "rs.format":rs_format, 'temp_dir':mkdirs(create_random_str()+'_ignore'), 'frames_cache':[] }
+        streams[stream['type']] = { 
+            **stream, 
+            "rs.type":rs_type, 
+            "rs.format":rs_format, 
+            'temp_dir':mkdirs(create_random_str()+'_ignore'), 
+            'frames_cache':[],
+            'video_writer':None
+        }
         if stream['type'] == 'stream.color': color_exists = True
         print(f"Adding stream {stream['type']} as dictated via command arguments")
 
@@ -91,113 +99,139 @@ frame_aligner = rs.align(align_to)
 # Initialize a colorizer to visualize the depth color
 frame_colorizer = rs.colorizer()
 
-# Read the streams, output what kind of stream types they each are
-profile = pipeline.start(config)
-gc.disable()
+try:
+    # Read the streams, output what kind of stream types they each are
+    profile = pipeline.start(config)
+    gc.disable()
 
-# Start iterating through frames
-print("Reading `.bag` data...")
-while True:
-    # Wait for frames. If frames don't exist anymore, exit early
-    frame_present, frames = pipeline.try_wait_for_frames()
-    if not frame_present:
-        print("Frames no longer present!")
-        break
+    # Start iterating through frames
+    print("Reading `.bag` data...")
+    while True:
+        # Wait for frames. If frames don't exist anymore, exit early
+        frame_present, frames = pipeline.try_wait_for_frames()
+        if not frame_present:
+            print("Frames no longer present!")
+            break
 
-    # Apply alignment
-    aligned_frames = frame_aligner.process(frames)
+        # Apply alignment
+        aligned_frames = frame_aligner.process(frames)
 
-    # Get aligned frames. We only extract ones dependent on available streams
+        # Get aligned frames. We only extract ones dependent on available streams
+        for stream_type in streams:
+            # get the stream itself
+            stream = streams[stream_type]
+            
+            # Handle different cases of stream types
+            if stream_type == 'stream.depth': 
+                # Get the image, and colorize since it's a depth image
+                _frame = aligned_frames.get_depth_frame()
+                color_frame = frame_colorizer.colorize(_frame)
+                _image = np.asanyarray(color_frame.get_data())
+                _frame_number = _frame.get_frame_number()
+                _frame_timestamp = _frame.get_timestamp()   # milliseconds
+            elif stream_type == 'stream.color': 
+                _frame = aligned_frames.get_color_frame()
+                _image = np.asanyarray(_frame.get_data())
+                _frame_number = _frame.get_frame_number()
+                _frame_timestamp = _frame.get_timestamp()   # milliseconds
+
+            # Resize the image to fit the output dimensions
+            resized_img = cv2.resize(_image, (args.output_dims[0], args.output_dims[1]), interpolation=cv2.INTER_AREA)
+
+            # Save and Cache
+            out_filename = os.path.join(stream['temp_dir'], f'{_frame_number}.png')
+            streams[stream_type]['frames_cache'].append([_frame_number, _frame_timestamp, out_filename])
+            cv2.imwrite(out_filename, resized_img)
+            
+        # Make sure to add a quitter operation for early exits
+        key = cv2.waitKey(1)
+        # if pressed escape exit program
+        if key == 27:
+            break
+
+    # Stop the pipeline
+    pipeline.stop()
+    profile = None
+
+    # Generate an output basename for each stream
+    filename_root, file_extension = os.path.splitext(args.input_bag)
+    dirname = os.path.dirname(filename_root)
+    filename = os.path.basename(filename_root) 
+
+    # Now, with each frame cache saved, let's sort each and then print them as actual videos
+    print("Generating videos...")
     for stream_type in streams:
-        # get the stream itself
+        # Get the stream info and the cache; sort that frame cache based on frame timestamp
         stream = streams[stream_type]
-        
-        # Handle different cases of stream types
-        if stream_type == 'stream.depth': 
-            # Get the image, and colorize since it's a depth image
-            _frame = aligned_frames.get_depth_frame()
-            color_frame = frame_colorizer.colorize(_frame)
-            _image = np.asanyarray(color_frame.get_data())
-            _frame_number = _frame.get_frame_number()
-            _frame_timestamp = _frame.get_timestamp()   # milliseconds
-        elif stream_type == 'stream.color': 
-            _frame = aligned_frames.get_color_frame()
-            _image = np.asanyarray(_frame.get_data())
-            _frame_number = _frame.get_frame_number()
-            _frame_timestamp = _frame.get_timestamp()   # milliseconds
+        s_frames_cache = sorted(stream['frames_cache'], key=lambda x: x[1])
+        temp_dir = stream['temp_dir']
 
-        # Resize the image to fit the output dimensions
-        resized_img = cv2.resize(_image, (args.output_dims[0], args.output_dims[1]), interpolation=cv2.INTER_AREA)
+        # Generate an output video filename
+        output_filename = os.path.join(dirname, filename+"_"+stream_type.split(".")[1]+".mp4")
 
-        # Save and Cache
-        out_filename = os.path.join(stream['temp_dir'], f'{_frame_number}.png')
-        streams[stream_type]['frames_cache'].append([_frame_number, _frame_timestamp, out_filename])
-        cv2.imwrite(out_filename, resized_img)
-           
-    # Make sure to add a quitter operation for early exits
-    key = cv2.waitKey(1)
-    # if pressed escape exit program
-    if key == 27:
-        break
+        # Generate a video writer. We rely on the same FPS as the original stream
+        streams[stream_type]['video_writer'] = cv2.VideoWriter(
+            output_filename, 
+            cv2.VideoWriter_fourcc(*'MP4V'), 
+            stream['fps'], 
+            [args.output_dims[0], args.output_dims[1]]
+        )
 
-# Stop the pipeline
-pipeline.stop()
-gc.enable()
+        # Iterate through sorted frames
+        prev_ts = None
+        frame_time = 1000 / stream['fps']
+        for frame in s_frames_cache:
+            img = cv2.imread(frame[2])
+            if img is None:
+                print(f"ERROR: Couldn't read image {frame[0]}")
+                continue
+            # if there's no previous timestamp, then we just add the image itself and set `prev_ts`
+            if prev_ts is None:
+                streams[stream_type]['video_writer'].write(img)
+                prev_ts = frame[1]
+                continue;
 
-# Generate an output basename for each stream
-filename_root, file_extension = os.path.splitext(args.input_bag)
-dirname = os.path.dirname(filename_root)
-filename = os.path.basename(filename_root) 
-
-# Now, with each frame cache saved, let's sort each and then print them as actual videos
-print("Generating videos...")
-for stream_type in streams:
-    # Get the stream info and the cache; sort that frame cache based on frame timestamp
-    stream = streams[stream_type]
-    s_frames_cache = sorted(stream['frames_cache'], key=lambda x: x[1])
-    temp_dir = stream['temp_dir']
-
-    # Generate an output video filename
-    output_filename = os.path.join(dirname, filename+"_"+stream_type.split(".")[1]+".mp4")
-
-    # Generate a video writer. We rely on the same FPS as the original stream
-    video_writer = cv2.VideoWriter(
-        output_filename, 
-        cv2.VideoWriter_fourcc(*'MP4V'), 
-        stream['fps'], 
-        [args.output_dims[0], args.output_dims[1]]
-    )
-
-    # Iterate through sorted frames
-    prev_ts = None
-    frame_time = 1000 / stream['fps']
-    for frame in s_frames_cache:
-        img = cv2.imread(frame[2])
-        if img is None:
-            print(f"ERROR: Couldn't read image {frame[0]}")
-            continue
-        # if there's no previous timestamp, then we just add the image itself and set `prev_ts`
-        if prev_ts is None:
-            video_writer.write(img)
+            # Calcualte delta time
+            dt = frame[1] - prev_ts
             prev_ts = frame[1]
-            continue;
 
-        # Calcualte delta time
-        dt = frame[1] - prev_ts
-        prev_ts = frame[1]
+            # Consecutively add frames until the next active frame
+            frames_to_write = max(1, round(dt / frame_time))
+            for _ in range(frames_to_write):
+                streams[stream_type]['video_writer'].write(img)
 
-        # Consecutively add frames until the next active frame
-        frames_to_write = max(1, round(dt / frame_time))
-        for _ in range(frames_to_write):
-            video_writer.write(img)
+        # Release writer
+        streams[stream_type]['video_writer'].release()
+        streams[stream_type]['video_writer'] = None
 
-    # Release writer
-    video_writer.release()
+        # Print results
+        print(f"\tOutput stream {output_filename} generated from {stream_type}!")
 
-    # Delete temp folder
-    shutil.rmtree(temp_dir)
+    # Print message to indicate completion
+    print("All streams outputted!")
 
-    # Print results
-    print(f"\tOutput stream {output_filename} generated from {stream_type}!")
+except Exception as e:
+    print(e)
 
-print("Done: All streams outputted!")
+finally:
+    # If pipeline is still running, stop it
+    if profile is not None:
+        pipeline.stop()
+        profile = None
+
+    # Enable GC
+    gc.enable()
+
+    # Delete temp folders and video writers for each stream
+    for stream_type in streams:
+        stream = streams[stream_type]
+
+        if os.path.exists(streams[stream_type]['temp_dir']):
+            shutil.rmtree(streams[stream_type]['temp_dir'])
+
+        if streams[stream_type]['video_writer'] is not None:
+            streams[stream_type]['video_writer'].release()
+            streams[stream_type]['video_writer'] = None
+    
+    # Print message to indicate closing
+    print("Closing generator!")
